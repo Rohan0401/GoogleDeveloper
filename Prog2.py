f6732e67af7fc7c1bd15f95d4bea6e8b75f855c8
Rohan0401 <rohan.rajput4@gmail.com>
2016-06-07 06:51:06 -0400

    My Commit

---
+++ Prog2.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris 
+iris = load_iris()
+import numpy as np
+from sklearn import tree
+test_idx = [0,50,100]
+
+#training data 
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx , axis = 0)
+
+#test data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data,train_target)
+
+print test_target
+clf.predict(test_data)
